4c8494a4956fbe35a8899ff188ba51fb8d000f5a
iftikhoq <iftikhirul16@gmail.com>
2024-03-15 23:24:33 +0600

    day1

---
+++ cp/Week1/Day1/A - Four Digits.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define ll              long long
+#define yes             cout << "YES\n";
+#define no              cout << "NO\n";
+#define f1(n)           for(int i=0;i<n;i++)
+#define f2(a,n)         for(int i=a;i<n;i++)
+#define vi              vector <long long>
+#define pb              push_back
+#define sp              << " "
+#define endl            "\n"
+#define MOD             1000000007
+
+using namespace std;
+void solve(){
+    int n;
+    cin >> n;
+    int t=n, count(0);
+    if(n==0){
+        cout << "0000";
+        return;
+    }
+    while(n) {
+        n/=10;
+        count++;
+    }
+    f1(4-count) cout << "0";
+    cout << t;
+}   
+signed main(){
+    ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+   
+   //  int test;
+   //  cin >> test;
+   //  while (test--) 
+            solve();
+}
---
+++ cp/Week1/Day1/A- Counting.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll              long long
+#define yes             cout << "YES\n";
+#define no              cout << "NO\n";
+#define f1(n)           for(int i=0;i<n;i++)
+#define f2(a,n)         for(int i=a;i<n;i++)
+#define vi              vector <long long>
+#define pb              push_back
+#define sp              << " "
+#define endl            "\n"
+#define MOD             1000000007
+
+using namespace std;
+void solve(){
+    ll a,b;
+    cin >> a >> b;
+    if(b-a+1<=0){
+        cout << "0\n";
+    }    
+    else{
+        cout << b-a+1 << endl;
+    }
+}   
+signed main(){
+    ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+   
+   //  int test;
+   //  cin >> test;
+   //  while (test--) 
+            solve();
+}
