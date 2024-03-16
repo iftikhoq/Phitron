3716e99b2a3f6eb179e12a889be6cfd4b2d0c8d9
iftikhoq <iftikhirul16@gmail.com>
2024-03-16 23:59:05 +0600

    day2

---
+++ cp/Week1/Day2/A - Biscuit Generator.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll              long long
+#define yes             cout << "YES\n";
+#define no              cout << "NO\n";
+#define f1(n)           for(int i=0;i<n;i++)
+#define f2(a,n)         for(int i=a;i<n;i++)
+#define vi              vector <long long>
+#define pb              push_back
+#define sp              << " "
+#define endl            "\n"
+#define MOD             1000000007
+
+using namespace std;
+void solve(){
+    int a,b,t;
+    cin >> a >> b >> t;
+    int total=0,c=1;
+    while(a*c<=t){
+        total+=b;
+        c++;
+    }
+    cout << total << endl;
+}   
+signed main(){
+    ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+   
+   //  int test;
+   //  cin >> test;
+   //  while (test--) 
+            solve();
+}
