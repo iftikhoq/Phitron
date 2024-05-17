eeef1288c07f1402c07afc95aa51f5fe3dd6defe
Iftikhirul Hoque <97209299+iftikhoq@users.noreply.github.com>
2024-05-18 00:02:16 +0600

    Day7

---
+++ cp/Week8/Day7/A_Maximum_GCD.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll              long long
+#define yes             cout << "YES\n";
+#define no              cout << "NO\n";
+#define f1(n)           for(int i=0;i<n;i++)
+#define f2(a,n)         for(int i=a;i<n;i++)
+#define vector <int>    vi
+
+using namespace std;
+void solve(){
+    int n;
+    cin >> n;
+
+    cout << n/2 << endl;
+}   
+int main() {
+    ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+   
+    int test;
+    cin >> test;
+    while (test--) 
+    solve();
+}
